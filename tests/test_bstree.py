de69c89b1916f30214a61eab42543c184cdc3ffc
Miguel Mendes <miguel.mdebrito@gmail.com>
2018-10-01 18:17:10 -0300

    Use pytest for tests

--- tests/test_bstree.py
+++ tests/test_bstree.py
@@ -1,20 +1,18 @@
-import unittest
-
 from pybstrees import BinarySearchTree
 
 
-class BinarySearchTreeTest(unittest.TestCase):
-    def test_empty_tree(self):
-        tree = BinarySearchTree()
-        self.assertFalse(tree)
+def test_empty_tree():
+    tree = BinarySearchTree()
+    assert not tree
 
-    def test_insert_on_empty_tree(self):
-        tree = BinarySearchTree()
-        tree.insert(9)
-        tree.insert(10)
-        tree.insert(8)
 
-        self.assertEqual(tree.root.entry, 9)
-        self.assertEqual(tree.root.left.entry, 8)
-        self.assertEqual(tree.root.right.entry, 10)
-        self.assertTrue(tree)
+def test_insert_on_empty_tree():
+    tree = BinarySearchTree()
+    tree.insert(9)
+    tree.insert(10)
+    tree.insert(8)
+
+    assert tree.root.entry == 9
+    assert tree.root.left.entry == 8
+    assert tree.root.right.entry == 10
+    assert tree
