9c9bbc44e6691ace70860594e812ba527989c097
Miguel Mendes <miguel.mdebrito@gmail.com>
2018-10-02 15:29:09 -0300

    Improve insertion test

--- tests/test_bstree.py
+++ tests/test_bstree.py
@@ -9,12 +9,17 @@ def test_empty_tree():
 def test_insert_on_empty_tree():
     tree = BinarySearchTree()
     tree.insert(9)
-    tree.insert(10)
-    tree.insert(8)
+    tree.insert(4)
+    tree.insert(14)
+    tree.insert(17)
+    tree.insert(7)
 
-    assert tree.root.entry == 9
-    assert tree.root.left.entry == 8
-    assert tree.root.right.entry == 10
+    root = tree.root
+    assert root.entry == 9
+    assert root.left.entry == 4
+    assert root.right.entry == 14
+    assert root.right.right.entry == 17
+    assert root.left.right.entry == 7
     assert tree
 
 
@@ -28,5 +33,3 @@ def test_insert_duplicated_entry():
     assert tree.root.right.entry == 10
     assert not tree.root.left
     assert tree
-
-    # elif entry > self.entry
