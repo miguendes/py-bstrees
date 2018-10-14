fba39176ed24b8f59151d5d5a41f65e8189c3dc4
Miguel Mendes <miguel.mdebrito@gmail.com>
2018-10-14 20:58:56 -0300

    Move AVLTree to project

--- pybstree.py
+++ pybstree.py
@@ -372,10 +372,20 @@ class _EmptyAVLNode:
         """Inserting a entry in a EmptyNode means returning a concrete node back."""
         return _AVLNode(entry)
 
+    def delete(self, entry):
+        """Cannot delete a entry from a EmptyNode"""
+        raise KeyError(entry)
+
     @property
     def balance_factor(self):
         """The balance factor of a empty node is always 0."""
         return 0
+
+    def pred(self, pred, entry):
+        raise KeyError(f'Predecessor of {entry} not found.')
+
+    def succ(self, pred, entry):
+        raise KeyError(f'Successor of {entry} not found.')
 
     def __bool__(self):
         """Empty node is always Falsy. """
@@ -399,9 +409,9 @@ class _AVLNode:
     def __init__(self, entry):
         """Creates a new node."""
         self.entry = entry
-        self.left = EMPTY_AVL_NODE
-        self.right = EMPTY_AVL_NODE
-        self.height = 1
+        self.left: '_AVLNode' = EMPTY_AVL_NODE
+        self.right: '_AVLNode' = EMPTY_AVL_NODE
+        self.height: int = 1
 
     def insert(self, entry):
         """Inserts a entry to the subtree."""
@@ -412,10 +422,76 @@ class _AVLNode:
 
         return self._balanced_tree()
 
+    def delete(self, entry):
+        """Deletes a entry from subtree and return it balanced."""
+        if entry > self.entry:
+            self.right = self.right.delete(entry)
+        elif entry < self.entry:
+            self.left = self.left.delete(entry)
+        else:
+            if self.is_leaf():
+                return EMPTY_NODE
+
+            if self.left:
+                new_entry = self.left.max()
+                self.entry = new_entry
+                self.left = self.left.delete(new_entry)
+            else:
+                new_entry = self.right.entry
+                self.entry = new_entry
+                self.right = self.right.delete(new_entry)
+
+        return self._balanced_tree()
+
+    def clear(self):
+        """Clears the whole subtree"""
+        if self.is_leaf():
+            return EMPTY_AVL_NODE
+        self.left = self.left.clear()
+        self.right = self.right.clear()
+
+        return EMPTY_AVL_NODE
+
+    def is_leaf(self):
+        """Checks if the node is a leaf node, i. e, if its siblings are empty."""
+        return not (bool(self.left) or bool(self.right))
+
+    def max(self):
+        """Returns the max element in the subtree."""
+        max_entry = self.entry
+        right_node = self.right
+        while right_node:
+            max_entry = right_node.entry
+            right_node = right_node.right
+
+        return max_entry
+
+    def min(self):
+        """Returns the min element in the subtree."""
+        min_entry = self.entry
+        left_node = self.left
+        while left_node:
+            min_entry = left_node.entry
+            left_node = left_node.left
+
+        return min_entry
+
     @property
-    def balance_factor(self) -> int:
+    def balance_factor(self):
         """Returns the balance factor of the node."""
         return self.left.height - self.right.height
+
+    def __bool__(self):
+        """Returns True if the node is not None"""
+        return self.entry is not None
+
+    def __len__(self):
+        """Return the number of elements in this subtree."""
+        return 1 + len(self.left) + len(self.right)
+
+    def __eq__(self, other):
+        """Checks if two nodes are equal."""
+        return self.entry == other.entry and self.left == other.left and self.right == other.right
 
     def _balanced_tree(self):
         """Returns balanced tree after balance operation."""
@@ -471,13 +547,37 @@ class _AVLNode:
         self.right = self.right._rotate_right()
         return self._rotate_left()
 
+    def pred(self, pred, entry):
+        if entry > self.entry:
+            return self.right.pred(self, entry)
+        elif entry < self.entry:
+            return self.left.pred(pred, entry)
+        else:
+            if self.left:
+                return self.left.max()
+            if pred:
+                return pred.entry
+
+            raise KeyError(f'Predecessor of {entry} not found.')
+
+    def succ(self, succ, entry):
+        if entry > self.entry:
+            return self.right.succ(succ, entry)
+        elif entry < self.entry:
+            return self.left.succ(self, entry)
+        else:
+            if self.right:
+                return self.right.min()
+            if succ:
+                return succ.entry
+
+            raise KeyError(f'Successor of {entry} not found.')
+
 
 class AVLTree:
     """
     AVLTree implements a balanced binary tree.
-
     Reference: http://en.wikipedia.org/wiki/AVL_tree
-
     In computer science, an AVL tree is a self-balancing binary search tree, and
     it is the first such data structure to be invented. In an AVL tree, the
     heights of the two child subtrees of any node differ by at most one;
@@ -488,23 +588,175 @@ class AVLTree:
     The AVL tree is named after its two inventors, G.M. Adelson-Velskii and E.M.
     Landis, who published it in their 1962 paper "An algorithm for the
     organization of information."
-
+    AVLTree expected comparable objects as entries.
+    AVLTree() -> new empty tree.
+    AVLTree(tree) -> new tree initialized from a tree
+    AVLTree(seq) -> new tree initialized from seq [(entry1), (entry2), ... (entryN)]
     """
 
-    def __init__(self):
+    def __init__(self, args=None):
         """Initialize an AVL Tree. """
-        self.root = EMPTY_AVL_NODE
+        self.root = None
+        self._init_tree(args)
 
     def insert(self, entry):
         """T.insert(entry) -- insert elem"""
         self.root = self.root.insert(entry)
 
-    def __bool__(self):
-        """Returns True if the tree is not empty"""
-        return bool(self.root)
+    def delete(self, entry):
+        """T.remove(entry) remove item <entry> from tree."""
+        self.root = self.root.delete(entry)
+
+    def traverse(self, order='inorder'):
+        """Traverse the tree based on a given strategy.
+        order : 'preorder' | 'postorder' | 'bfs' | default 'inorder'
+            The traversal of the tree.
+            Use 'preorder' to print the root first, then left and right subtree, respectively.
+            Use 'postorder' to print the left and right subree first, then the root.
+            Use 'bfs' to visit the tree in a breadth-first manner.
+            The default is 'inorder' which prints the left subtree, the root and the right subtree.
+        """
+        if order == 'preorder':
+            return self._preorder(self.root)
+        elif order == 'postorder':
+            return self._postorder(self.root)
+        elif order == 'bfs':
+            return self._bfs()
+        else:
+            return self._inorder(self.root)
 
     @property
     def height(self):
         """Returns the height of the tree. When the tree is empty its height is zero."""
         return self.root.height
 
+    def search(self, entry):
+        """Returns k if T has a entry k, else raise KeyError"""
+        return self._search(entry).entry
+
+    def _search(self, entry):
+        """Returns node.k if T has a entry k, else raise KeyError"""
+        root = self.root
+
+        while root:
+            if entry > root.entry:
+                root = root.right
+            elif entry < root.entry:
+                root = root.left
+            else:
+                return root
+
+        raise KeyError(f'Entry {entry} not found.')
+
+    def pred(self, entry):
+        return self.root.pred(EMPTY_NODE, entry)
+
+    def succ(self, entry):
+        return self.root.succ(EMPTY_NODE, entry)
+
+    def __len__(self):
+        """T.__len__() <==> len(x). Retuns the number of elements in the tree."""
+        return len(self.root)
+
+    def __contains__(self, entry):
+        """k in T -> True if T has a entry k, else False"""
+        try:
+            self.search(entry)
+            return True
+        except KeyError:
+            return False
+
+    def max(self):
+        """T.max() -> get the maximum entry of T."""
+        return self.root.max()
+
+    def min(self):
+        """T.min() -> get the minimum entry of T."""
+        return self.root.min()
+
+    def clear(self):
+        """T.clear() -> Removes all entries of T leaving it empty."""
+        self.root = self.root.clear()
+
+    def __repr__(self):
+        """T.__repr__(...) <==> repr(x).
+        Returns representation of the object that can be used to recreate the tree with the same values."""
+        return f'{self.__class__.__name__}({list(self._bfs())})'
+
+    def __str__(self):
+        """T.__str__(...) <==> str(x)."""
+        return repr(self)
+
+    def __eq__(self, other):
+        """Checks if two trees are equal. """
+        if isinstance(other, self.__class__):
+            if self.height == other.height and len(self) == len(other):
+                return self.root == other.root
+        return False
+
+    def __bool__(self):
+        """Returns True if the tree is not empty"""
+        return bool(self.root)
+
+    def __copy__(self):
+        """Returns a shallow copy of the tree."""
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
+
+    def _init_tree(self, args):
+        """Initialize the tree according to the arguments passed. """
+        self.root = EMPTY_AVL_NODE
+
+        if args is not None:
+            if isinstance(args, self.__class__):
+                args = args.traverse('bfs')
+
+            try:
+                for entry in args:
+                    self.insert(entry)
+            except (ValueError, TypeError) as e:
+                raise TypeError('AVLTree constructor called with '
+                                f'incompatible data type: {e}')
+
+    def _inorder(self, root):
+        """Performs an in-order traversal. """
+        if root:
+            yield from self._inorder(root.left)
+            yield root.entry
+            yield from self._inorder(root.right)
+
+    def _preorder(self, root):
+        """Performs an pre-order traversal."""
+        if root:
+            yield root.entry
+            yield from self._preorder(root.left)
+            yield from self._preorder(root.right)
+
+    def _postorder(self, root):
+        """Performs an post-order traversal."""
+        if root:
+            yield from self._postorder(root.left)
+            yield from self._postorder(root.right)
+            yield root.entry
+
+    def _bfs(self):
+        """Performs an Breadth first traversal."""
+        root = self.root
+
+        if root:
+            q = deque()
+            q.append(root)
+
+            while q:
+                root = q.popleft()
+                if not root:
+                    continue
+
+                yield root.entry
+                left = root.left
+                right = root.right
+
+                q.append(left)
+                q.append(right)
--- tests/test_bstree.py
+++ tests/test_bstree.py
@@ -394,6 +394,559 @@ class TestAVLTree:
         assert tree.root.entry == 9
         assert tree.root.right.entry == 14
 
+    def test_recursive_insertion(self, tree):
+        tree.insert(9)
+        tree.insert(4)
+        tree.insert(14)
+        tree.insert(17)
+        tree.insert(7)
+
+        root = tree.root
+        assert root.entry == 9
+        assert root.left.entry == 4
+        assert root.right.entry == 14
+        assert root.right.right.entry == 17
+        assert root.left.right.entry == 7
+
+    def test_height(self, tree):
+        tree.insert(9)
+        root = tree.root
+        assert root.height == 1
+
+        tree.insert(4)
+        assert root.height == 2
+        assert root.left.height == 1
+
+        tree.insert(14)
+        assert root.height == 2
+        assert root.left.height == 1
+        assert root.right.height == 1
+
+        tree.insert(17)
+        assert root.height == 3
+        assert root.left.height == 1
+        assert root.right.height == 2
+        assert root.right.right.height == 1
+
+        tree.insert(7)
+        assert root.height == 3
+        assert root.left.height == 2
+        assert root.left.right.height == 1
+
+    def test_balance_factor(self, tree):
+        tree.insert(9)
+        root = tree.root
+        assert root.balance_factor == 0
+
+        tree.insert(4)
+        assert root.balance_factor == 1
+        assert root.left.balance_factor == 0
+
+        tree.insert(14)
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == 0
+
+        tree.insert(17)
+        assert root.balance_factor == -1
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == -1
+        assert root.right.right.balance_factor == 0
+
+        tree.insert(7)
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == -1
+        assert root.left.right.balance_factor == 0
+
+    def test_single_left_rotation(self, tree):
+        tree.insert(1)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 1
+
+        tree.insert(2)
+        root = tree.root
+        assert root.balance_factor == -1
+        assert root.height == 2
+        assert root.entry == 1
+        assert root.right.entry == 2
+
+        tree.insert(3)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == 0
+        assert root.height == 2
+
+        assert root.entry == 2
+        assert root.left.entry == 1
+        assert root.right.entry == 3
+
+    def test_single_right_rotation(self, tree):
+        tree.insert(3)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 3
+
+        tree.insert(2)
+        root = tree.root
+        assert root.balance_factor == 1
+        assert root.height == 2
+        assert root.entry == 3
+        assert root.left.entry == 2
+
+        tree.insert(1)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == 0
+        assert root.height == 2
+
+        assert root.entry == 2
+        assert root.left.entry == 1
+        assert root.right.entry == 3
+
+    def test_left_right_rotation(self, tree):
+        tree.insert(3)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 3
+
+        tree.insert(1)
+        root = tree.root
+        assert root.balance_factor == 1
+        assert root.height == 2
+        assert root.entry == 3
+        assert root.left.entry == 1
+
+        tree.insert(2)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == 0
+        assert root.height == 2
+
+        assert root.entry == 2
+        assert root.left.entry == 1
+        assert root.right.entry == 3
+
+    def test_right_left_rotation(self):
+        tree = AVLTree()
+        tree.insert(1)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 1
+
+        tree.insert(3)
+        root = tree.root
+        assert root.balance_factor == -1
+        assert root.height == 2
+        assert root.entry == 1
+        assert root.right.entry == 3
+
+        tree.insert(2)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.left.balance_factor == 0
+        assert root.right.balance_factor == 0
+        assert root.height == 2
+
+        assert root.entry == 2
+        assert root.left.entry == 1
+        assert root.right.entry == 3
+
+    def test_advanced_right_rotation(self):
+        tree = AVLTree()
+        tree.insert(8)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 8
+
+        tree.insert(5)
+        root = tree.root
+
+        assert root.balance_factor == 1
+        assert root.height == 2
+        assert root.entry == 8
+        assert root.left.entry == 5
+
+        tree.insert(11)
+        root = tree.root
+
+        assert root.balance_factor == 0
+        assert root.height == 2
+        assert root.entry == 8
+        assert root.right.entry == 11
+
+        tree.insert(4)
+        root = tree.root
+
+        assert root.balance_factor == 1
+        assert root.height == 3
+        assert root.entry == 8
+        assert root.left.left.entry == 4
+
+        tree.insert(7)
+        root = tree.root
+
+        assert root.balance_factor == 1
+        assert root.height == 3
+        assert root.entry == 8
+        assert root.left.right.entry == 7
+
+        tree.insert(2)
+        root = tree.root
+
+        assert root.balance_factor == 0
+        assert root.height == 3
+        assert root.entry == 5
+        assert root.left.entry == 4
+        assert root.right.entry == 8
+        assert root.left.left.entry == 2
+        assert root.right.right.entry == 11
+        assert root.right.left.entry == 7
+
+    def test_advanced_left_rotation(self):
+        tree = AVLTree()
+        tree.insert(20)
+        root = tree.root
+        assert root.balance_factor == 0
+        assert root.height == 1
+        assert root.entry == 20
+
+        tree.insert(10)
+        root = tree.root
+
+        assert root.balance_factor == 1
+        assert root.height == 2
+        assert root.entry == 20
+        assert root.left.entry == 10
+
+        tree.insert(25)
+        root = tree.root
+
+        assert root.balance_factor == 0
+        assert root.height == 2
+        assert root.entry == 20
+        assert root.right.entry == 25
+
+        tree.insert(23)
+        root = tree.root
+
+        assert root.balance_factor == -1
+        assert root.height == 3
+        assert root.entry == 20
+        assert root.right.left.entry == 23
+
+        tree.insert(29)
+        root = tree.root
+
+        assert root.balance_factor == -1
+        assert root.height == 3
+        assert root.entry == 20
+        assert root.right.right.entry == 29
+
+        tree.insert(30)
+        root = tree.root
+
+        assert root.balance_factor == 0
+        assert root.height == 3
+        assert root.entry == 25
+        assert root.left.entry == 20
+        assert root.right.entry == 29
+        assert root.left.left.entry == 10
+        assert root.right.right.entry == 30
+        assert root.left.right.entry == 23
+
+    def test_traversal(self, tree):
+        tree.insert(20)
+        tree.insert(10)
+        tree.insert(25)
+        tree.insert(23)
+        tree.insert(29)
+        tree.insert(30)
+
+        d = {
+            'preorder': (25, 20, 10, 23, 29, 30),
+            'inorder': (10, 20, 23, 25, 29, 30),
+            'postorder': (10, 23, 20, 30, 29, 25),
+            'bfs': (25, 20, 29, 10, 23, 30),
+        }
+
+        for order, expected_value in d.items():
+            assert tuple(tree.traverse(order)) == expected_value
+
+    def test_empty_traversal(self, tree):
+        d = {
+            'preorder': (),
+            'inorder': (),
+            'postorder': (),
+            'bfs': (),
+        }
+
+        for order, expected_value in d.items():
+            assert tuple(tree.traverse(order)) == expected_value
+
+    def test_length(self, tree):
+        entries = range(150)
+        for entry in entries:
+            tree.insert(entry)
+        assert len(tree) == len(entries)
+        assert len(AVLTree()) == 0
+
+    def test_contains(self):
+        assert 10 not in AVLTree()
+
+        tree = AVLTree()
+        tree.insert(10)
+
+        assert 10 in tree
+
+        entries = range(128)
+
+        tree = AVLTree()
+
+        for entry in entries:
+            tree.insert(entry)
+
+        for entry in entries:
+            assert entry in tree
+
+    def test_balanced_tree_must_have_height_of_log2(self):
+        import math
+        base = 2
+
+        for exp in range(2, 13):
+            tree1 = AVLTree()
+            tree2 = AVLTree()
+            tree3 = AVLTree()
+
+            entries = [i for i in range(base ** exp)]
+            for entry in entries:
+                tree1.insert(entry)
+                if entry == 0:
+                    continue
+                tree2.insert(entry)
+                if entry == 1:
+                    continue
+                tree3.insert(entry)
+
+            assert tree1.height == int(math.log2(len(entries))) + 1
+            assert len(tree1) == len(entries)
+            assert tree2.height == int(math.log2(len(entries)))
+            assert len(tree2) == len(entries) - 1
+            assert tree3.height == int(math.log2(len(entries)))
+            assert len(tree3) == len(entries) - 2
+
+    def test_initialize_tree_from_sequence(self):
+        import math
+        entries = [1, 2, 3, 4, 5, 6, 7]
+        tree = AVLTree(entries)
+
+        expected_order = (4, 2, 6, 1, 3, 5, 7)
+        assert tuple(tree.traverse('bfs')) == expected_order
+        assert len(tree) == len(entries)
+        assert tree.height == math.ceil(math.log2(len(entries)))
+
+    def test_constructor_not_properly_called(self):
+        with pytest.raises(TypeError) as context:
+            AVLTree(4)
+        assert ("AVLTree constructor called with incompatible data type: "
+                "'int' object is not iterable" in str(context.value))
+
+    def test_find_max(self):
+        entries = get_random_entries()
+        tree = AVLTree(entries)
+        assert tree.max() == max(entries)
+
+    def test_find_min(self):
+        entries = get_random_entries()
+        tree = AVLTree(entries)
+        assert tree.min() == min(entries)
+
+    def test_delete_single_element(self):
+        tree = AVLTree([1])
+
+        tree.delete(1)
+
+        assert 1 not in tree
+        assert not tree
+        assert len(tree) == 0
+
+    def test_delete_leaf_node(self):
+        tree = AVLTree([1, 2])
+
+        tree.delete(2)
+
+        assert 1 in tree
+        assert 2 not in tree
+        assert tree
+        assert len(tree) == 1
+
+    def test_delete_not_existent_entry(self):
+        self.assert_entry_error([1, 2, 3], 10)
+        self.assert_entry_error(None, 10)
+
+    def assert_entry_error(self, entries, entry_to_be_deleted):
+        with pytest.raises(KeyError) as context:
+            tree = AVLTree(entries)
+            tree.delete(entry_to_be_deleted)
+            assert f"entryError: {entry_to_be_deleted}" in str(context.value)
+
+    def test_delete_entry_but_tree_remains_balanced(self):
+        entries = [10, 5, 11, 3, 7, 15]
+        tree = AVLTree(entries)
+        entry_to_be_deleted = 10
+
+        tree.delete(entry_to_be_deleted)
+
+        expected_order = (7, 5, 11, 3, 15)
+        assert entry_to_be_deleted not in tree
+        assert tuple(tree.traverse('bfs')) == expected_order
+
+    def test_delete_entry_make_tree_unbalanced(self):
+        entries = [5, 3, 8, 2, 4, 7, 11, 1, 6, 10, 12, 9]
+        tree = AVLTree(entries)
+        entry_to_be_deleted = 4
+
+        tree.delete(entry_to_be_deleted)
+
+        expected_order = (8, 5, 11, 2, 7, 10, 12, 1, 3, 6, 9)
+        assert entry_to_be_deleted not in tree
+        assert tuple(tree.traverse('bfs')) == expected_order
+
+    def test_delete_entries_in_a_row(self):
+        entries = [2, 1, 4, 3, 5]
+        tree = AVLTree(entries)
+
+        tree.delete(1)
+        assert 1 not in tree
+        assert tuple(tree.traverse('bfs')), (4, 2, 5 == 3)
+
+        tree.delete(2)
+        assert 2 not in tree
+        assert tuple(tree.traverse('bfs')), (4, 3 == 5)
+
+        tree.delete(3)
+        assert 3 not in tree
+        assert tuple(tree.traverse('bfs')), (4 == 5)
+
+        tree.delete(4)
+        assert 4 not in tree
+        assert tuple(tree.traverse('bfs')) == (5,)
+
+        tree.delete(5)
+        assert 5 not in tree
+        assert tuple(tree.traverse('bfs')) == ()
+        assert not tree
+
+    def test_str_repr(self):
+        tree = AVLTree([1, 2, 3, 4, 5])
+        assert repr(tree), 'AVLTree([2, 1, 4, 3 == 5])'
+        assert str(tree), 'AVLTree([2, 1, 4, 3 == 5])'
+
+    def test_equals(self):
+        tree1 = AVLTree([1, 2, 3, 4, 5])
+        tree2 = AVLTree([2, 1, 4, 3, 5])
+        tree3 = AVLTree([1, 2, 3, 4, 5, 6])
+
+        assert tree1 == tree2
+        assert tree1 is not tree2
+        assert tree1 != tree3
+        assert tree1 is not tree3
+        assert tree2 != tree3
+        assert tree2 is not tree3
+        assert tree1 != int(9)
+        assert tree1 != AVLTree()
+
+    def test_clear(self):
+        tree = AVLTree([1, 2, 3, 4, 5])
+        tree.clear()
+
+        assert not tree
+
+    def test_build_tree_from_other(self):
+        original = AVLTree([1, 2, 3, 4, 5])
+        copy = AVLTree(original)
+
+        assert copy == AVLTree([2, 1, 4, 3, 5])
+
+    def test_search(self):
+        tree = AVLTree([1, 2, 3, 4, 5])
+        entry = tree.search(4)
+
+        assert 4 == entry
+
+    def test_search_complex_data_type(self):
+        tree = AVLTree([Entry(1, 'a'),
+                        Entry(2, 'b'),
+                        Entry(3, 'c'),
+                        Entry(3, 'd'), ])
+        entry = tree.search(Entry(3, 'd'))
+
+        assert Entry(3, 'd') == entry
+
+        entry = Entry(3113, 'd')
+        with pytest.raises(KeyError) as context:
+            tree.search(entry)
+        assert f"Entry {entry} not found." in str(context.value)
+
+    def test_copy(self):
+        import copy
+        single_entry = Entry(1, ['a'])
+        tree1 = AVLTree([single_entry,
+                         Entry(2, 'b'),
+                         Entry(3, 'c'),
+                         Entry(3, 'd'), ])
+        tree2 = copy.copy(tree1)
+        assert tree1 == tree2
+        single_entry.b = 'a'
+        assert tree1 == tree2
+
+    def test_pred(self):
+        import random
+        random.seed(7477)
+        entries = get_random_entries()
+        tree = AVLTree(entries)
+
+        pred, prev = None, None
+        for entry in tree.traverse():
+            try:
+                pred = tree.pred(entry)
+            except KeyError:
+                assert prev is None
+            assert prev == pred
+            prev = entry
+
+        with pytest.raises(KeyError) as context:
+            tree.pred(1000000)
+        assert "Predecessor of 1000000 not found." in str(context.value)
+
+    def test_succ(self):
+        import random
+        random.seed(7477)
+        entries = get_random_entries()
+        tree = AVLTree(entries)
+        rev_entries = sorted(entries, reverse=True)
+
+        succ, prev = None, None
+        for entry in rev_entries:
+            try:
+                succ = tree.succ(entry)
+            except KeyError:
+                assert prev is None
+            assert prev == succ
+            prev = entry
+
+        with pytest.raises(KeyError) as context:
+            tree.succ(1000000)
+        assert "Successor of 1000000 not found." in str(context.value)
+
 
 def get_random_entries():
     from random import randint, shuffle, seed
